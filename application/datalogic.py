f40e77a79b1fdfb4872ffec5bfbb488fbadc9606
Jannis Jahr <jannis@Janniss-MacBook-Pro.local>
2017-06-18 10:33:22 +0200

    implemented methods for datalogic, implemented statusclass in statuslogic, changed database

--- application/datalogic.py
+++ application/datalogic.py
@@ -2,17 +2,66 @@ import json
 from shutil import copyfile
 import os
 
+### INTERFACES 
+# connector to get values out of database
+class DatabaseGet:
+	# initialize DatabaseGet with given database
+	def __init__(self, database):
+		self._database = database
+
+	# get whole database
+	def getData(self, args):
+		return self._database.getConfiguration(args)
+
+	# returns integer of status with highest id
+	def getHighestId(self):
+		statusList = self.getStatus()
+		highest = -1
+		for status in statusList:
+			if(status['statusId'] > highest):
+				highest = status['statusId']
+		return highest
+
+	# gets status of database. if no status id given, return all
+	def getStatus(self, statusId = None):
+		statusList = self._database.getConfiguration('statusList')
+		if(not statusId):
+			return statusList
+		for status in _return:
+			if(statusId == status['statusId']):
+				return status
+		return None
+
+
+# connector to write values into database
+class DatabaseWrite:
+	def __init__(self, database):
+		self._database = Database
+
+	def writeData(self, args, value):
+		return _database.writeConfiguration(args, value)
+
+### INTERFACES
+
+# Database
 class Database:	
 	def __init__(self, base = "configuration", userbase = "configuration/user", file = "database.json"):
 		self.base = base
 		self.file = file
 		self.userbase = userbase
-		self.dataOperations = DataOperations(base, userbase, file)
-		self.dataOperations.createDatabase()
+		self._dataOperations = DataOperations(base, userbase, file)
+		self._dataOperations.createDatabase()
+
+	# get configuration with optional given args. args can be a string or an array of strings, that iterates through the dictionary
+	def getConfiguration(self, args = None):
+		return self._dataOperations.returnConfiguration(args)
+
+	def writeConfiguration(self, args, value):
+		return self._dataOperations.writeConfiguration(args, value)
 
 # Todo Put in data operations
 class DataOperations:
-	def __init__(self, base, userbase, file):
+	def __init__(self, base = "configuration", userbase = "configuration/user", file = "database.json"):
 		self.base = base
 		self.userbase = userbase
 		self.file = file
@@ -36,6 +85,54 @@ class DataOperations:
 		else:
 			print("Did not create file, because already existing")
 
+	def keyError(self):
+		return {'error': 'no data matching'}
 
-		
-data = Database()
+	def returnConfiguration(self, args = None):
+		# store json in temporary object
+		with open(self.userbasefile) as f:
+			data = json.load(f)
+			# if there are no arguments given, give all data
+			try:
+				if(not args):
+					return data
+				# if it is list, suppose that args are in array form
+				if(isinstance(args, list)):
+					return self._getByGivenArgs(args, data)
+					print("B")
+				return data[args]
+			except(KeyError):
+				return self.keyError()
+
+	def writeConfiguration(self, args, value):
+		# open database temporary
+		with open(self.userbasefile) as f:
+			data = json.load(f)
+			if(isinstance(args, list)):
+				return True
+			data[args] = value
+		# save data to database finally
+		self.saveToDatabase(data)
+		return False
+
+	def saveToDatabase(self, data):
+		with open(self.userbasefile, 'w') as outfile:
+			json.dump(data, outfile, indent=2)
+			return True
+
+	def _setByGivenArgs(self, args, data, value):
+		print("not implemented")
+		return None
+
+
+	def _getByGivenArgs(self, args, data):
+		# nested dict with multiple args in array form
+		current_data = data
+		for arg in args:
+			current_data = current_data[arg]
+		return current_data
+
+# TODO: rausnehmen am Ende
+database = Database()
+dataConnect = DatabaseGet(database)
+print(dataConnect.getHighestId())
--- application/statuslogic.py
+++ application/statuslogic.py
@@ -2,16 +2,18 @@ import serial
 import subprocess
 import time
 import threading
-import datalogic
+from datalogic import DatabaseGet
 
+# Sensor: Allows connection to a hardware Sensor
 class Sensor:
+	# name = name of usb host of sensor
+	# rate = baud rate of sensor
 	def __init__(self, name, rate):
 		self.name = name
 		self.rate = rate
 		self.thread = None
 		self.connection_error = False
 		self.serial = self.getSerial()
-		self.return_value = None
 		self.status = None
 		self.thread = None
 		self.statusRequestPending = False
@@ -19,6 +21,7 @@ class Sensor:
 		self.maximalRequests = 5
 		self.startListeningThread()
 
+	# build serial connection to sensor
 	def getSerial(self):
 		try:
 			return serial.Serial(self.name,self.rate)
@@ -26,6 +29,7 @@ class Sensor:
 			print("No Serial Device was connected.")
 			self.connection_error = True
 
+	# listen to changes of sensor
 	def listenToChanges(self):
 		# reads status and returns it when finished
 		if(not self.connection_error):
@@ -36,21 +40,25 @@ class Sensor:
 					self.statusRequestPending = False
 				self.status = currentValue
 
+	# check if there was a connection error at the beginning
 	def hasConnectionError(self):
-		# check if there is a connection error
 		return self.connection_error
 
+	# starts listening thread to sensor
 	def startListeningThread(self):
 		if(not self.hasConnectionError()):
 			self.thread = threading.Thread(target=self.listenToChanges, args=())
 			self.thread.daemon = True
 			self.thread.start()
 
+	# requests the status by sending a byte to the hardware sensor
 	def requestStatusOnce(self):
 		if(not self.hasConnectionError()):
 			self.statusRequestPending = True
 			self.serial.write('1')
 
+	# make multiple requests until answer is given
+	# TODO: implement timeout
 	def makeStatusRequest(self):
 		self.statusRequestPending = True
 		currentRequest = 1
@@ -61,19 +69,27 @@ class Sensor:
 			time.sleep(.1)
 		return True
 
+	# return an error if could not connect
 	def returnError(self):
 		return {"error": "Device not available"}
 
+	# make a status request and return value
 	def getCurrentStatus(self):
 		if(self.hasConnectionError()):
 			self.returnError()
 		self.makeStatusRequest()
 		return self.status
 
-class DataInterpreter:
-	def __init__(self):
-		#Todo
-		self.databaseConnector = None
+class Status:
+	# sensor = sensor object
+	# statusId = id of sensor - automatically generated
+	def __init__(self, sensor, databaseGet):
+		self.sensor = sensor
+		self.statusId = generateStatusId()
+		self.databaseGet = databaseGet
 
-	def interpretRawData(self, data):
+	# generate status id by looking up the highest value in database and incrementing
+	def generateStatusId():
+		return databaseGet.getHighestId() + 1
+
 
