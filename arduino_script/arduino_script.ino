354ea4a5758bf207314d23239535384181790cdf
Jannis Jahr <jannis@Janniss-MBP.multi.box>
2017-06-28 09:16:57 +0200

    Changed headline

--- arduino_script/arduino_script.ino
+++ arduino_script/arduino_script.ino
@@ -1,15 +1,6 @@
-/* Analog Read to LED
- * ------------------ 
- *
- * turns on and off a light emitting diode(LED) connected to digital  
- * pin 13. The amount of time the LED will be on and off depends on
- * the value obtained by analogRead(). In the easiest case we connect
- * a potentiometer to analog pin 2.
- *
- * Created 1 December 2005
- * copyleft 2005 DojoDave <http://www.0j0.org>
- * http://arduino.berlios.de
- *
+/*
+ * Arduino Sensor Request
+ * by Jannis Jahr, 2017
  */
 
 int potPin = 2;    // select the input pin for the potentiometer (Analog)
